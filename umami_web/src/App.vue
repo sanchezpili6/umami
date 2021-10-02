58d32bfa1ba68411613f975a34787ecbcd79fe1b
MrCasco <ernesto.casco.velazquez@hotmail.com>
2021-10-02 13:20:11 -0500

    Footer done

--- umami_web/src/App.vue
+++ umami_web/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <v-app>
     <router-view></router-view>
+    <Footer></Footer>
   </v-app>
 </template>
 
 
 <script>
+import Footer from "./components/Footer";
+
 export default {
   name: 'App',
+  components: {
+    Footer,
+  },
   data: () => ({
-    //
+
   }),
 };
 </script>
