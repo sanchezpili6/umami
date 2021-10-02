68b905783bfe50a2bc2ed061dbe1108daa45260b
Jacqueline Andres <jacky.andres@hotmail.com>
2021-10-02 04:53:47 -0500

    Achievements Page to review

---
+++ umami_web/src/components/Achievements/Hongos.vue
@@ -0,0 +1,52 @@
+<template>
+<v-sheet
+    class="mx-auto"
+    elevation="0"
+    max-width="800"
+  >
+     <!--HONGOS-->
+    <v-slide-group
+      v-model="model"
+      class="pa-4"
+      show-arrows
+    >
+      <v-slide-item v-for="hongo in hongos" :key="hongo" v-slot="{ active, toggle }">
+        <v-card
+          :color="active ? 'primary' : 'grey lighten-1'"
+          class="ma-3"
+          height="200"
+          width="200"
+          @click="toggle"
+        >
+        <v-img :src="hongo.image" class="fill-height"></v-img>
+        </v-card>
+      </v-slide-item>
+    </v-slide-group>
+
+    <v-expand-transition>
+      <v-sheet v-if="model != null" height="200" tile>
+        <v-row class="fill-height" align="center" justify="center">
+          <h3 class="text-h6">
+              {{hongos[model].nombre}}
+          </h3>
+        </v-row>
+      </v-sheet>
+    </v-expand-transition>
+</v-sheet>
+</template>
+
+
+<script>
+  export default {
+    data: () => ({
+        model:null,
+        hongos: [
+        { nombre: 'Matamoscas o Falsa Oronja', nombreCientifico: 'Amanita muscaria', image: 'https://www.gastronomiavasca.net/uploads/image/file/6294/amanita_muscaria1.jpg'},
+        { nombre: 'Hongo Velo de Novia', nombreCientifico: 'Phallus indusiatus', image: 'https://media.istockphoto.com/photos/bridal-veil-fungi-picture-id923913436?k=20&m=923913436&s=170667a&w=0&h=xdh8jMN4UGC-YzF3u8WOQsuVK5-uoWhDPegGn8W4QhE='},
+        { nombre: 'Hongo Langosta', nombreCientifico: 'Hypomyces lactifluorum', image: 'https://i.etsystatic.com/18376711/r/il/2ef200/1656013834/il_570xN.1656013834_2kkr.jpg'},
+        { nombre: 'Hongo Pambazo', nombreCientifico: 'Funghi porcini', image: 'https://inaturalist-open-data.s3.amazonaws.com/photos/5628160/medium.jpg?1480281163'},
+        { nombre: 'Pedos de burro', nombreCientifico: 'Lycoperdon perlatum', image: 'http://antropocene.it/wp-content/uploads/2019/03/Lycoperdon_perlatum.jpg'}
+        ]
+        }),
+  }
+</script>
---
+++ umami_web/src/components/Achievements/Zonas.vue
@@ -0,0 +1,55 @@
+<template>
+<v-sheet
+    class="mx-auto"
+    elevation="0"
+    max-width="800"
+  >
+  <!--ZONAS-->
+    <v-container>
+        <p class="text-h4">ZONAS</p>
+    </v-container>
+    <v-slide-group
+      v-model="model"
+      class="pa-4"
+      show-arrows
+    >
+      <v-slide-item v-for="zona in zonas" :key="zona" v-slot="{ active, toggle }">
+        <v-card
+          :color="active ? 'primary' : 'grey lighten-1'"
+          class="ma-3"
+          height="200"
+          width="200"
+          @click="toggle"
+        >
+        <v-img :src="zona.image" class="fill-height"></v-img>
+        </v-card>
+      </v-slide-item>
+    </v-slide-group>
+
+    <v-expand-transition>
+      <v-sheet v-if="model != null" height="200" tile>
+        <v-row class="fill-height" align="center" justify="center">
+          <h3 class="text-h6">
+              {{zonas[model].nombre}}
+          </h3>
+        </v-row>
+      </v-sheet>
+    </v-expand-transition>
+</v-sheet>
+</template>
+
+
+<script>
+  export default {
+    data: () => ({
+        model:null,
+         zonas: [
+        { nombre: 'Veracruz', image: 'https://pbs.twimg.com/profile_images/1145710581934120961/f_PoGn-T.png'},
+        { nombre: 'Jalisco', image: 'https://m.media-amazon.com/images/I/71hiD7WeGGL._AC_SL1500_.jpg'},
+        { nombre: 'Estado de México', image: 'https://www.nicepng.com/png/detail/369-3698095_escudo-del-estado-de-mexico-png-escudo-del.png'},
+        { nombre: 'Querétaro', image: 'https://lh3.googleusercontent.com/proxy/s24AGQzEUsSxAjrzFmUfChW0a_t79qdy94rNeTW2lUds0lTBK4Rq9feEwjQarUrCoSmhRGNpj2zmuo8EpJKREKl87WxVUqTfAGSZBU822csoSeEMYhQOmS85XDp2NGstHztmwG0ZOudIdqr-mxiJ16e_aA'},
+        { nombre: 'Guanajuato', image: 'https://www.gob.mx/cms/uploads/document/main_image/34821/guanajuato_escudo.jpg'}
+        ]
+        }),
+  }
+</script>
